6477b6b91b4aa1becdbbc219be50424a2b7c72f3
Thangncph26123 <congthang25092003@gmail.com>
2022-11-02 00:13:14 +0700

    commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,12 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 /**
  *
  * @author thangncph26123
  */
+
 public class main {
-    
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
 }
