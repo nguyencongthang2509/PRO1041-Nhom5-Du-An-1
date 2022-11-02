980e0f7df1c7a7ad5146e3850cfd736ee21e85a3
nguyencongthang2509 <congthang25092003@gmail.com>
2022-11-02 17:09:52 +0700

    aaaa

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,5 +9,10 @@ public class main {
     
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
