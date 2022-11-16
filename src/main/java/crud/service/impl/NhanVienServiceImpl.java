d8a0a36daa6d22df7f192efee460a85705b201a6
LONGHOANG2303\ADMIN <longhoang0323@gmail.com>
2022-11-16 22:43:10 +0700

    CRUD NhanVien

---
+++ src/main/java/crud/service/impl/NhanVienServiceImpl.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crud.service.impl;
+
+import crud.repository.NhanVienRepository;
+import crud.service.NhanVienService;
+import domainmodels.NhanVien;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author longnhph26222
+ */
+public class NhanVienServiceImpl implements NhanVienService {
+
+    private NhanVienRepository repo = new NhanVienRepository();
+
+    @Override
+    public List<NhanVien> getList() {
+        return repo.getList();
+    }
+
+    @Override
+    public String insert(NhanVien nv) {
+        boolean check = repo.saveorUpdate(nv);
+        if (nv.getMa().isEmpty()) {
+            return "Mã không được trống";
+        }
+        NhanVien nvfind = repo.findbyMa(nv.getMa());
+        if (nvfind != null) {
+            return "Mã không được trùng";
+        }
+        if (nv.getTen().isEmpty()) {
+            return "Tên không được trống";
+        }
+        if (nv.getDiaChi().isEmpty()) {
+            return "Địa chỉ không được trống";
+        }
+        if (nv.getEmail().isEmpty()) {
+            return "Email không được trống";
+        }
+        if (nv.getGioiTinh().isEmpty()) {
+            return "Giới tính không được trống";
+        }
+        if (nv.getMatKhau().isEmpty()) {
+            return "Mật khẩu không được trống";
+        }
+        if (nv.getSdt().isEmpty()) {
+            return "SĐT không được trống";
+        }
+        if (nv.getSdt().matches("^0{1}\\d{10}$")) {
+            return "Số điện thoại phải là số và gồm 10 ký tự";
+        }
+        if (check) {
+            return "Thêm thành công";
+        }
+        return "Thêm thất bại";
+    }
+
+    @Override
+    public String update(NhanVien nv) {
+        if (nv.getMa().isEmpty()) {
+            return "Mã không được trống";
+        }
+        NhanVien nvfindbyID = repo.findbyID(nv.getId());
+        if (nvfindbyID == null) {
+            return "Nhân viên này không tồn tại";
+        }
+        if (!nv.getMa().equals(nvfindbyID.getMa())) {
+            NhanVien nvfindbyMa = repo.findbyMa(nv.getMa());
+            if (nvfindbyMa != null) {
+                return "Mã không được trùng";
+            }
+            nvfindbyID.setMa(nv.getMa());
+        }
+        if (nv.getTen().isEmpty()) {
+            return "Tên không được trống";
+        }
+        if (nv.getDiaChi().isEmpty()) {
+            return "Địa chỉ không được trống";
+        }
+        if (nv.getEmail().isEmpty()) {
+            return "Email không được trống";
+        }
+        if (nv.getGioiTinh().isEmpty()) {
+            return "Giới tính không được trống";
+        }
+        if (nv.getMatKhau().isEmpty()) {
+            return "Mật khẩu không được trống";
+        }
+        if (nv.getSdt().isEmpty()) {
+            return "SĐT không được trống";
+        }
+        if (nv.getSdt().matches("^0{1}\\d{10}$")) {
+            return "Số điện thoại phải là số và gồm 10 ký tự";
+        }
+        boolean check = repo.saveorUpdate(nvfindbyID);
+        if (check) {
+            return "Sửa thành công";
+        }
+        return "Sửa thất bại";
+    }
+
+    @Override
+    public String delete(UUID id) {
+        NhanVien nv = repo.findbyID(id);
+        if(nv == null){
+            return "Nhân viên này không tồn tại";
+        }
+        boolean check = repo.delete(nv);
+        if (check) {
+            return "Xóa thành công";
+        }
+        return "Xóa thất bại";
+    }
+
+}
