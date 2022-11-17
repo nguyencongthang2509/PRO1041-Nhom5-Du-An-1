702d9ea9f6c34d67e7d7c5c479007d66d1c6ca44
nguyencongthang2509 <congthang25092003@gmail.com>
2022-11-17 17:05:10 +0700

    phác thảo giao diện

---
+++ src/main/java/view/JFrameQuanLy.java
@@ -0,0 +1,258 @@
+package view;
+
+/**
+ *
+ * @author thangncph26123
+ */
+public class JFrameQuanLy extends javax.swing.JFrame {
+
+    public JFrameQuanLy() {
+        initComponents();
+        setLocationRelativeTo(null);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        btnBanHang = new com.k33ptoo.components.KButton();
+        jLabel1 = new javax.swing.JLabel();
+        btnSanPham = new com.k33ptoo.components.KButton();
+        kButton11 = new com.k33ptoo.components.KButton();
+        kButton12 = new com.k33ptoo.components.KButton();
+        kButton13 = new com.k33ptoo.components.KButton();
+        kButton14 = new com.k33ptoo.components.KButton();
+        kButton15 = new com.k33ptoo.components.KButton();
+        kButton16 = new com.k33ptoo.components.KButton();
+        btnHoaDon = new com.k33ptoo.components.KButton();
+        jPanel2 = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(153, 204, 255));
+
+        btnBanHang.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        btnBanHang.setText("Bán hàng");
+        btnBanHang.setToolTipText("");
+        btnBanHang.setFont(new java.awt.Font("Roboto Slab Black", 0, 24)); // NOI18N
+        btnBanHang.setkBackGroundColor(new java.awt.Color(255, 204, 204));
+        btnBanHang.setkEndColor(new java.awt.Color(153, 153, 255));
+        btnBanHang.setkHoverEndColor(new java.awt.Color(255, 102, 102));
+        btnBanHang.setkHoverForeGround(new java.awt.Color(204, 204, 255));
+        btnBanHang.setkHoverStartColor(new java.awt.Color(204, 204, 255));
+        btnBanHang.setkSelectedColor(new java.awt.Color(255, 153, 153));
+        btnBanHang.setkStartColor(new java.awt.Color(255, 153, 153));
+
+        jLabel1.setBackground(new java.awt.Color(153, 204, 255));
+
+        btnSanPham.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        btnSanPham.setText("Sản phẩm");
+        btnSanPham.setFont(new java.awt.Font("Roboto Slab Black", 0, 24)); // NOI18N
+        btnSanPham.setkBackGroundColor(new java.awt.Color(255, 204, 204));
+        btnSanPham.setkEndColor(new java.awt.Color(153, 153, 255));
+        btnSanPham.setkHoverEndColor(new java.awt.Color(255, 102, 102));
+        btnSanPham.setkHoverForeGround(new java.awt.Color(204, 204, 255));
+        btnSanPham.setkHoverStartColor(new java.awt.Color(204, 204, 255));
+        btnSanPham.setkSelectedColor(new java.awt.Color(255, 153, 153));
+        btnSanPham.setkStartColor(new java.awt.Color(255, 153, 153));
+
+        kButton11.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        kButton11.setText("Khuyến mại");
+        kButton11.setFont(new java.awt.Font("Roboto Slab Black", 0, 24)); // NOI18N
+        kButton11.setkBackGroundColor(new java.awt.Color(255, 204, 204));
+        kButton11.setkEndColor(new java.awt.Color(153, 153, 255));
+        kButton11.setkHoverEndColor(new java.awt.Color(255, 102, 102));
+        kButton11.setkHoverForeGround(new java.awt.Color(204, 204, 255));
+        kButton11.setkHoverStartColor(new java.awt.Color(204, 204, 255));
+        kButton11.setkSelectedColor(new java.awt.Color(255, 153, 153));
+        kButton11.setkStartColor(new java.awt.Color(255, 153, 153));
+
+        kButton12.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        kButton12.setText("Đổi hàng");
+        kButton12.setFont(new java.awt.Font("Roboto Slab Black", 0, 24)); // NOI18N
+        kButton12.setkBackGroundColor(new java.awt.Color(255, 204, 204));
+        kButton12.setkEndColor(new java.awt.Color(153, 153, 255));
+        kButton12.setkHoverEndColor(new java.awt.Color(255, 102, 102));
+        kButton12.setkHoverForeGround(new java.awt.Color(204, 204, 255));
+        kButton12.setkHoverStartColor(new java.awt.Color(204, 204, 255));
+        kButton12.setkSelectedColor(new java.awt.Color(255, 153, 153));
+        kButton12.setkStartColor(new java.awt.Color(255, 153, 153));
+
+        kButton13.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        kButton13.setText("Thông kê");
+        kButton13.setFont(new java.awt.Font("Roboto Slab Black", 0, 24)); // NOI18N
+        kButton13.setkBackGroundColor(new java.awt.Color(255, 204, 204));
+        kButton13.setkEndColor(new java.awt.Color(153, 153, 255));
+        kButton13.setkHoverEndColor(new java.awt.Color(255, 102, 102));
+        kButton13.setkHoverForeGround(new java.awt.Color(204, 204, 255));
+        kButton13.setkHoverStartColor(new java.awt.Color(204, 204, 255));
+        kButton13.setkSelectedColor(new java.awt.Color(255, 153, 153));
+        kButton13.setkStartColor(new java.awt.Color(255, 153, 153));
+
+        kButton14.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        kButton14.setText("Nhân viên");
+        kButton14.setFont(new java.awt.Font("Roboto Slab Black", 0, 24)); // NOI18N
+        kButton14.setkBackGroundColor(new java.awt.Color(255, 204, 204));
+        kButton14.setkEndColor(new java.awt.Color(153, 153, 255));
+        kButton14.setkHoverEndColor(new java.awt.Color(255, 102, 102));
+        kButton14.setkHoverForeGround(new java.awt.Color(204, 204, 255));
+        kButton14.setkHoverStartColor(new java.awt.Color(204, 204, 255));
+        kButton14.setkSelectedColor(new java.awt.Color(255, 153, 153));
+        kButton14.setkStartColor(new java.awt.Color(255, 153, 153));
+
+        kButton15.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        kButton15.setText("Khách hàng");
+        kButton15.setFont(new java.awt.Font("Roboto Slab Black", 0, 24)); // NOI18N
+        kButton15.setkBackGroundColor(new java.awt.Color(255, 204, 204));
+        kButton15.setkEndColor(new java.awt.Color(153, 153, 255));
+        kButton15.setkHoverEndColor(new java.awt.Color(255, 102, 102));
+        kButton15.setkHoverForeGround(new java.awt.Color(204, 204, 255));
+        kButton15.setkHoverStartColor(new java.awt.Color(204, 204, 255));
+        kButton15.setkSelectedColor(new java.awt.Color(255, 153, 153));
+        kButton15.setkStartColor(new java.awt.Color(255, 153, 153));
+
+        kButton16.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        kButton16.setText("Đăng xuất");
+        kButton16.setFont(new java.awt.Font("Roboto Slab Black", 0, 24)); // NOI18N
+        kButton16.setkBackGroundColor(new java.awt.Color(255, 204, 204));
+        kButton16.setkEndColor(new java.awt.Color(153, 153, 255));
+        kButton16.setkHoverEndColor(new java.awt.Color(255, 102, 102));
+        kButton16.setkHoverForeGround(new java.awt.Color(204, 204, 255));
+        kButton16.setkHoverStartColor(new java.awt.Color(204, 204, 255));
+        kButton16.setkSelectedColor(new java.awt.Color(255, 153, 153));
+        kButton16.setkStartColor(new java.awt.Color(255, 153, 153));
+
+        btnHoaDon.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        btnHoaDon.setText("Hóa đơn");
+        btnHoaDon.setFont(new java.awt.Font("Roboto Slab Black", 0, 24)); // NOI18N
+        btnHoaDon.setkBackGroundColor(new java.awt.Color(255, 204, 204));
+        btnHoaDon.setkEndColor(new java.awt.Color(153, 153, 255));
+        btnHoaDon.setkHoverEndColor(new java.awt.Color(255, 102, 102));
+        btnHoaDon.setkHoverForeGround(new java.awt.Color(204, 204, 255));
+        btnHoaDon.setkHoverStartColor(new java.awt.Color(204, 204, 255));
+        btnHoaDon.setkSelectedColor(new java.awt.Color(255, 153, 153));
+        btnHoaDon.setkStartColor(new java.awt.Color(255, 153, 153));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(btnBanHang, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(btnSanPham, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(kButton11, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(kButton12, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(kButton13, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(kButton14, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(kButton15, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(kButton16, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(btnHoaDon, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {btnBanHang, btnHoaDon, btnSanPham, kButton11, kButton12, kButton13, kButton14, kButton15, kButton16});
+
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnBanHang, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnHoaDon, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnSanPham, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(kButton11, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(kButton12, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(kButton13, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(kButton14, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(kButton15, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(kButton16, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel2.setBackground(new java.awt.Color(153, 204, 255));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1161, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(JFrameQuanLy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(JFrameQuanLy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(JFrameQuanLy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JFrameQuanLy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JFrameQuanLy().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.k33ptoo.components.KButton btnBanHang;
+    private com.k33ptoo.components.KButton btnHoaDon;
+    private com.k33ptoo.components.KButton btnSanPham;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private com.k33ptoo.components.KButton kButton11;
+    private com.k33ptoo.components.KButton kButton12;
+    private com.k33ptoo.components.KButton kButton13;
+    private com.k33ptoo.components.KButton kButton14;
+    private com.k33ptoo.components.KButton kButton15;
+    private com.k33ptoo.components.KButton kButton16;
+    // End of variables declaration//GEN-END:variables
+}
